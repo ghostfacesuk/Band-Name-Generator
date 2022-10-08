75f3b76aed451546e27734d7153eb3e3d82b5ae7
Steve G <ssghostfacekillah@hotmail.com>
2022-10-08 12:38:40 +0100

    Add full stop in title

--- Band_Name_Generator.py
+++ Band_Name_Generator.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band Name Generator\n")
+print("Welcome to the Band Name Generator.\n")
 city_name = input("What city did you grow up in?\n")
 pet_name = input("\nWhat is your pet's name?\n")
 print("\nYour band name is " + city_name + " " + pet_name + "!")
